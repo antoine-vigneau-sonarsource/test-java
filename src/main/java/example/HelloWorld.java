9f7c333ce1a9ec5de3aacff17b18d87a934a8c1a
Antoine Vigneau <antoine.vigneau@sonarsource.com>
2019-01-01 00:00:00 +0100

    Initial master commit 01/01

---
+++ src/main/java/example/HelloWorld.java
@@ -0,0 +1,13 @@
+package example;
+
+public class HelloWorld {
+
+  public void coveredByUnitTest1() {
+    System.out.println("coveredByUnitTest1");
+  }
+
+  public void notCoveredByUnitTest1() {
+    System.out.println("notCoveredByUnitTest1");
+  }
+
+}
---
+++ src/test/java/example/HelloWorldUnitTest.java
@@ -0,0 +1,12 @@
+package example;
+
+import org.junit.Test;
+
+public class HelloWorldUnitTest {
+
+  @Test
+  public void test1() {
+    new HelloWorld().coveredByUnitTest1();
+  }
+
+}
